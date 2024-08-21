0f7bbcd2cb8d9657f0b2e996c3cb28bcdd7f2ed6
Camilo Sevillano <kamilo-50@hotmail.com>
2024-08-21 15:40:35 -0500

    Creacion y conexion de templates internamente como externamente e integracion de TailwindCSS

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProductFroms
+from .models import Product
 
 # Create your views here.
 
@@ -13,3 +14,9 @@ class ProductFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+    
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
