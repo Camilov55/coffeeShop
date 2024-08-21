0eaf5d0a7b5c5d31af8da6ba3c7529bc00a3b62e
Camilo Sevillano <kamilo-50@hotmail.com>
2024-08-20 22:51:46 -0500

    Gestion de models y gestion de relaciones de Muchos a Muchos (N:N)

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,32 @@ from django.db import models
 class Car(models.Model):
     title = models.TextField(max_length=250)
     year = models.TextField(max_length=4, null=True)
+
+    def  __str__(self):
+        return f"{self.title} - {self.year}"
+
+
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birth_data = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_data = models.DateField()
+    Publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name="authors")
+
+    def __str__(self):
+        return self.title
