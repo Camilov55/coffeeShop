2b008a4e3fce39ee8c20f24b5b8d0de3360d2c3b
Camilo Sevillano <kamilo-50@hotmail.com>
2024-08-21 10:52:41 -0500

    NUEVO PROYECTO

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import my_view, my_text_view, CarListView
-
-urlpatterns = [
-    #path("listado/", my_view),
-    path("listado/", CarListView.as_view()),
-    path("detalle/<int:id>", my_text_view),
-    path("marcas/<str:brand>", my_text_view)
-]
