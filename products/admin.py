8e302f516efb56c58dbc15b323e914554ede2fc3
Camilo Sevillano <kamilo-50@hotmail.com>
2024-08-21 16:24:56 -0500

    El migrate es la correccion de un error. Ingreso el al Admin de Django que viene por defecto

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from products.models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+    list_display = ['name', 'price'] # Los datos que quiere que aparezca de al inicio
+    search_fields = ['name'] # Genera un buscador de solo buscara en name del producto
+
+admin.site.register(Product, ProductAdmin)
